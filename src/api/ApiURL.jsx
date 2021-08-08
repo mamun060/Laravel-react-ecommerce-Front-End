fec752a8bdc2a404b29d502ee4c472f7562460f4
mamun060 <almamun.raju520@gmail.com>
2021-08-09 01:26:58 +0600

    REST API consume for about page

--- src/api/ApiURL.jsx
+++ src/api/ApiURL.jsx
@@ -1,6 +1,9 @@
 class ApiURL{
     static BaseURL="http://127.0.0.1:8000/api/"
     static  VisitorDetails=this.BaseURL+"SendVisitorDetails"
+    static SendContactDetails=this.BaseURL+"SendContactDetails"
+
+    static SendSiteInfo=this.BaseURL+"SendSiteInfo"
 }
 
 export default ApiURL;
--- src/component/common/Contact - Copy.jsx
+++ src/component/common/Contact - Copy.jsx
@@ -1,49 +1,53 @@
-import React, {Component, Fragment} from 'react';
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
-import validation from "../../validation/validation";
-import axios from "axios";
-import ApiURL from "../../api/ApiURL";
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React, { Component, Fragment } from 'react';
+import {Container, Row, Col, Button, Form} from 'react-bootstrap';
+import validation from '../../validation/validation';
+import axios from 'axios';
+import ApiURL from '../../api/ApiURL';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+import {ToastContainer, toast } from 'react-toastify';
+
 
 class Contact extends Component {
 
-    constructor() {
+    constructor(){
         super();
         this.state={
-            name:"",
-            mobile:"",
-            msg:""
+            name: "",
+            phone: "",
+            mgs: ""
         }
     }
 
     nameOnChange=(event)=>{
-      let name=  event.target.value;
-      this.setState({name:name})
+        let name= event.target.value;
+        this.setState({name:name})
     }
 
     mobileOnChange=(event)=>{
-        let mobile=  event.target.value;
+        let mobile= event.target.value;
         this.setState({mobile:mobile})
     }
-    msgOnChange=(event)=>{
-        let msg=  event.target.value;
-        this.setState({msg:msg})
+
+    messageOnChange=(event)=>{
+        let mgs= event.target.value;
+        this.setState({mgs:mgs})
     }
 
     onFormSubmit=(event)=>{
-
         let name=this.state.name;
         let mobile=this.state.mobile;
         let msg=this.state.msg;
         let sendBtn=document.getElementById('sendBtn');
         let contactForm=document.getElementById('contactForm');
 
+        // eslint-disable-next-line eqeqeq
         if(name.length==0){
             toast.error("Name Required",{
                 position:"bottom-center"
             });
         }
+        // eslint-disable-next-line eqeqeq
         else if(mobile.length==0){
             toast.error("Mobile Number Required",{
                 position:"bottom-center"
@@ -60,6 +64,7 @@ class Contact extends Component {
                 position:"bottom-center"
             });
         }
+        // eslint-disable-next-line eqeqeq
         else if(msg.length==0){
             toast.error("Please Write Your Message",{
                 position:"bottom-center"
@@ -93,40 +98,41 @@ class Contact extends Component {
                 sendBtn.innerHTML="Send"
             })
         }
+    event.preventDefault();
+  }
 
-
-        event.preventDefault();
-    }
-
-
-    render() {
-        return (
+    
+    render() { 
+        return ( 
             <Fragment>
                 <Container className="TopSection">
+          
                     <Row className="p-2">
                         <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
                             <Row className="text-center ">
                                 <Col className="d-flex justify-content-center" md={6} lg={6} sm={12} xs={12}>
-                                    <Form id="contactForm" onSubmit={this.onFormSubmit} className="onboardForm">
+                                     <Form id="contactForm" onSubmit={this.onFormSubmit} className="onboardForm">
                                         <h4 className="section-title">CONTACT WITH US</h4>
                                         <h6 className="section-sub-title">Please Enter Your Mobile No, And Go Next</h6>
                                         <input onChange={this.nameOnChange} className="form-control m-2" type="text" placeholder="Your Name"/>
                                         <input onChange={this.mobileOnChange} className="form-control m-2" type="text" placeholder="Mobile Number"/>
-                                        <input onChange={this.msgOnChange} className="form-control m-2" type="text" placeholder="Message"/>
+                                        <input onChange={this.messageOnChange} className="form-control m-2" type="text" placeholder="Message"/>
                                         <Button id="sendBtn" type="submit" className="btn btn-block m-2 site-btn">Send</Button>
                                     </Form>
                                 </Col>
                                 <Col className="p-0 Desktop m-0" md={6} lg={6} sm={6} xs={6}>
-                                    <iframe className="GoogleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.9009669046704!2d90.38828391450218!3d23.750910694674705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8bcd681372b%3A0x5c2b8755e4327004!2sBashundhara%20City%20Shopping%20Complex!5e0!3m2!1sen!2sbd!4v1601135282389!5m2!1sen!2sbd" ></iframe>
+                                <iframe className="GoogleMap"
+                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d645.6976191258681!2d90.42184931432053!3d23.725356067809372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b85020dde33b%3A0x70581a30763056f6!2z4Ka54KeA4Kaw4Ka-4Kad4Ka_4KayIOCmueCni-Cmn-Cnh-Cmsg!5e0!3m2!1sbn!2sbd!4v1623695068098!5m2!1sbn!2sbd" width="600" height="450" allowfullscreen="" loading="lazy">
+                                </iframe>
                                 </Col>
                             </Row>
                         </Col>
                     </Row>
-                    <ToastContainer />
+                   
                 </Container>
             </Fragment>
-        );
+         );
     }
 }
-
+ 
 export default Contact;
